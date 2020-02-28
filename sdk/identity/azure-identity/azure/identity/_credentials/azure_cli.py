8b130dd4e23029470832e0d4192607f70087e21a
Charles Lowell <chlowe@microsoft.com>
2020-02-28 13:41:44 -0800

    refactor

--- sdk/identity/azure-identity/azure/identity/_credentials/azure_cli.py
+++ sdk/identity/azure-identity/azure/identity/_credentials/azure_cli.py
@@ -2,9 +2,9 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 # ------------------------------------
+from datetime import datetime
 import json
 import time
-from datetime import datetime
 
 from subprocess import run, PIPE
 import six
@@ -12,47 +12,45 @@ import six
 from azure.core.credentials import AccessToken
 from azure.core.exceptions import ClientAuthenticationError
 
+from .._internal import _scopes_to_resource
+
+_CLI_NOT_INSTALLED_ERR = "Azure CLI not installed"
+_CLI_LOGIN_ERR = "ERROR: Please run 'az login' to setup account.\r\n"
+
+_COMMAND_LINE = "az account get-access-token --output json --resource {}"
+
 
 class AzureCliCredential(object):
-
-    _DEFAULT_PREFIX = "/.default"
-    _CLI_NOT_INSTALLED_ERR = "Azure CLI not installed"
-    _CLI_LOGIN_ERR = "ERROR: Please run 'az login' to setup account.\r\n"
-
-    def get_token(self, *scopes, **kwargs): # pylint:disable=unused-argument
-        command = 'az account get-access-token'
-        
-        if scopes:
-            resource = scopes[0]
-            if resource.endswith(self._DEFAULT_PREFIX):
-                resource = resource[:-len(self._DEFAULT_PREFIX)]
-
-            command = ' '.join([command, '--resource', resource])
+    def get_token(self, *scopes, **kwargs):  # pylint:disable=unused-argument
+        resource = _scopes_to_resource(*scopes)
+        command = _COMMAND_LINE.format(resource)
 
         try:
-            get_access_token_stdout = self._get_cli_access_token(command)
+            get_access_token_stdout = self._run_command(command)
             get_access_token_object = json.loads(get_access_token_stdout)
-            access_token = get_access_token_object['accessToken']
+            access_token = get_access_token_object["accessToken"]
         except ClientAuthenticationError:
             raise
         except Exception as e:
             raise ClientAuthenticationError(repr(e))
-        
-        expires_on = int((
-            datetime.strptime(get_access_token_object['expiresOn'], '%Y-%m-%d %H:%M:%S.%f')
-                - datetime.now()
-            ).total_seconds() + time.time())
+
+        expires_on = int(
+            (
+                datetime.strptime(get_access_token_object["expiresOn"], "%Y-%m-%d %H:%M:%S.%f") - datetime.now()
+            ).total_seconds()
+            + time.time()
+        )
 
         return AccessToken(access_token, expires_on)
 
-    def _get_cli_access_token(self, command):
-        _proc = run(command, shell=True, stderr=PIPE, stdout=PIPE, timeout=10)
-        return_code = _proc.returncode
-        stdout = six.ensure_str(_proc.stdout)
-        stderr = six.ensure_str(_proc.stderr)
-        if return_code == 127 or (return_code == 1 and 'not recognized as' in stderr):
+    def _run_command(self, command):
+        proc = run(command, shell=True, stderr=PIPE, stdout=PIPE, timeout=10)
+        return_code = proc.returncode
+        stdout = six.ensure_str(proc.stdout)
+        stderr = six.ensure_str(proc.stderr)
+        if return_code == 127 or (return_code == 1 and "not recognized as" in stderr):
             raise ClientAuthenticationError(self._CLI_NOT_INSTALLED_ERR)
         elif return_code == 1:
             raise ClientAuthenticationError(self._CLI_LOGIN_ERR)
-        
+
         return stdout
